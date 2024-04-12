5270af10bf922f888fdbf07b9d82dd349f727674
4rnv <arnavsdandekar@gmail.com>
2024-04-12 20:46:49 +0530

    fsdfsd

---
+++ index.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, send_from_directory, make_response
+from logik import get_wiki_content, get_wiki_content_title, save_content_to_pdf
+import threading
+import time
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/download', methods=['POST'])
+def userquery():
+    query = request.form['query'].strip()
+    font_style = request.form['font_style'].strip()
+    page_theme = request.form['theme'].strip()
+    if len(query) > 32:
+        return 'Your search is too long.'
+    content = get_wiki_content(query)
+    content_title = get_wiki_content_title(query)
+    timestamp = int(time.time()*1000000)
+    filename = f"{timestamp}.pdf"
+    if content==False:
+        return 'No such Wikipedia page exists.'
+    save_content_to_pdf(content, content_title, f"./pdf/{filename}", font_style, page_theme)
+    pdf_folder = os.path.join(app.root_path, 'pdf')
+    if not os.path.exists(pdf_folder):
+        os.makedirs(pdf_folder)
+    file_path = os.path.join(pdf_folder, filename)
+    delete_file_later(file_path, 300)
+    returning = make_response(send_from_directory(directory=pdf_folder, path=filename, as_attachment=False))
+    returning.headers['Content-Type'] = 'application/pdf'
+    returning.headers['Content-Disposition'] = f'inline; filename={filename}'
+    return returning
+
+def delete_file_later(path, delay):
+    def delete():
+        time.sleep(delay) # Keep file on server for some time
+        os.remove(path)
+
+    thread = threading.Thread(target=delete)
+    thread.start()
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ logik.py
@@ -0,0 +1,83 @@
+import wikipediaapi
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+import textwrap
+
+headers = {'User-Agent': 'Kaname/1.0; (https://funthingsare.fun; 7emen8wlk@mozmail.com)'}
+wiki_wiki = wikipediaapi.Wikipedia('en', headers=headers)
+
+def get_wiki_content(page_title):
+    page_py = wiki_wiki.page(page_title)
+    if len(page_py.text)==0:
+        return False
+    else:
+        return page_py.text
+
+def get_wiki_content_title(page_title):
+    page_py = wiki_wiki.page(page_title)
+    return page_py.title.upper()
+
+def save_content_to_pdf(text, title, filename, font_style, page_theme):
+    c = canvas.Canvas(filename, pagesize=letter)
+    width, height = letter  # Should I add A4 format option?
+    font_name = font_style
+    font_size = 10
+    title_font_size = 14
+    margin = 32
+    y_position = height - margin
+    if font_style == 'Times-Roman':
+        wrap_width = 120
+    if font_style == 'Helvetica':
+        wrap_width = 108
+    if font_style == 'Courier':
+        wrap_width = 84
+    c.setTitle(title)
+    c.setAuthor('Kaname/1.0')
+    # Initialize Y position and line height
+    line_height = font_size * 1.5
+    if page_theme == 'parch':
+        c.setFillColorRGB(0.96,0.90,0.71)
+        c.rect(0,0,width,height,fill=1)
+        c.setFillColorRGB(0.25,0.14,0.08)
+    elif page_theme == 'dark':
+        c.setFillColorRGB(0.10,0.10,0.10)
+        c.rect(0,0,width,height,fill=1)
+        c.setFillColorRGB(0.90,0.90,0.90)
+    else:
+        c.setFillColorRGB(0.95,0.95,0.95)
+        c.rect(0,0,width,height,fill=1)
+        c.setFillColorRGB(0,0,0)
+    c.setFont(font_name, title_font_size)
+    title_width = c.stringWidth(title, font_name, title_font_size)
+    title_x_position = (width - title_width)/2
+    c.drawString(title_x_position, y_position, title)
+    y_position -= (line_height+margin/2)
+    # Set font for the canvas
+    c.setFont(font_name, font_size)
+    
+    for paragraph in text.split('\n'):
+        wrapped_text = textwrap.fill(paragraph, wrap_width)
+        for line in wrapped_text.split('\n'):
+            if y_position < margin:  # Check to go on new page
+                c.showPage()
+                if page_theme == 'parch':
+                    c.setFillColorRGB(0.96,0.90,0.71)
+                    c.rect(0,0,width,height,fill=1)
+                    c.setFillColorRGB(0.25,0.14,0.08)
+                elif page_theme == 'dark':
+                    c.setFillColorRGB(0.10,0.10,0.10)
+                    c.rect(0,0,width,height,fill=1)
+                    c.setFillColorRGB(0.90,0.90,0.90)
+                else:
+                    c.setFillColorRGB(0.95,0.95,0.95)
+                    c.rect(0,0,width,height,fill=1)
+                    c.setFillColorRGB(0,0,0)
+                c.setFont(font_name, font_size)
+                y_position = height - margin  # Reset y_position
+            
+            c.drawString(margin, y_position, line)
+            y_position -= line_height  # Move to the next line
+        
+        y_position -= line_height * 0.8
+    
+    c.save()
